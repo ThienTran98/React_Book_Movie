7ac79900665205645d834d397c1ea5228b0bd05a
TranThien <tranhuuthien98@gmail.com>
2023-06-15 16:20:09 +0700

    update

--- src/services/configURL.js
+++ src/services/configURL.js
@@ -8,7 +8,6 @@ export const base_URL = axios.create({
   headers: {
     TokenCybersoft:
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJOb2RlanMgMjgiLCJIZXRIYW5TdHJpbmciOiIwNi8wOC8yMDIzIiwiSGV0SGFuVGltZSI6IjE2OTEyODAwMDAwMDAiLCJuYmYiOjE2NzI5MzgwMDAsImV4cCI6MTY5MTQyNzYwMH0.1IXShq-PS4U5xC7QUMQLQcPPHNDmZrXfqvEBkJOEvEw",
-
     Authorization: "Bearer " + userLocalStorage.get()?.accessToken,
   },
 });
